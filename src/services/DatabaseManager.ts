700a77d5c7e67c25f9e6794e9faf58f67ba4eea6
unchainedDavid <afonso.script.sol@gmail.com>
2023-03-25 21:43:31 -0400

    refactor: removing promises from database service

--- src/services/DatabaseManager.ts
+++ src/services/DatabaseManager.ts
@@ -7,111 +7,93 @@ import { IExercise } from "../database/Users/interfaces";
 import { IExerciseResponse, ILogResponse, IUserResponse } from "../interfaces/IResponses";
 
 class DatabaseManager {
-  createUser(username: string): Promise<IUserResponse> {
-    return new Promise((resolve, reject) => {
-      User.create({username})
-      .then( user => {
-        resolve({
-          username: user.username,
-          _id: user._id.toString()
-        });
-      })
-      .catch( error => reject(error))
-    })
-  }
-
-  getAllUsers(): Promise<IUserResponse[]> {
-    return new Promise((resolve, reject) => {
-      User.find()
-      .then( data => {
-        let users = Array<IUserResponse>();
-
-        for (let user of data) {
-          users.push({
-            _id: user._id.toString(),
-            username: user.username
-          })
-        }
-
-        resolve(users);
-      })
-      .catch( error => reject(error))
-    })
-  }
-
-  saveUserExercise(id: string, description: string, duration: number, date?: string): Promise<IExerciseResponse> {
-    let now = new Date();
-    let exercise: IExercise = {
-      description,
-      duration,
-      date: date ?? now.toDateString()
+  async createUser(username: string): Promise<IUserResponse> {
+    try {
+      const createdUser = await User.create({ username });      
+      return {
+        username: createdUser.username,
+        _id: createdUser._id.toString()
+      }
+    } catch (error) {
+      throw error;
     }
+  }
 
-    return new Promise((resolve, reject) => {
-      if (!mongoose.isValidObjectId(id)) {
-        return reject(new Error("Invalid user ID."));
+  async getAllUsers(): Promise<IUserResponse[]> {
+    try {
+      const usersFromDB = await User.find();
+      return usersFromDB.map((user): IUserResponse => {
+        return {
+          _id: user._id.toString(),
+          username: user.username
+        }
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async saveUserExercise(id: string, description: string, duration: number, date?: string): Promise<IExerciseResponse> {
+    const now = new Date();
+
+    // TODO: Put this validation on controllers
+    if (!mongoose.isValidObjectId(id)) 
+    throw new Error("Invalid user ID.");
+    
+
+    try {
+      const user = await User.findById(id);
+      const updatedUser = await user!.addExercise({
+        description,
+        duration,
+        date: date ?? now.toDateString()
+      });
+
+      return {
+        username: updatedUser.username,
+        description: updatedUser.exercises[updatedUser.exercises.length-1].description,
+        duration: updatedUser.exercises[updatedUser.exercises.length-1].duration,
+        date: updatedUser.exercises[updatedUser.exercises.length-1].date,
+        _id: updatedUser._id.toString()
       }
 
-      User.findById(id)
-      .then( user => {
-        return user?.addExercise({
-            description: exercise.description,
-            duration: exercise.duration,
-            date: exercise.date
-        })
-      })
-      .then( user => {
-        if (!user) return reject(new Error("User does not exist."));
-
-        resolve({
-          username: user.username,
-          description: user.exercises[user.exercises.length-1].description,
-          duration: user.exercises[user.exercises.length-1].duration,
-          date: user.exercises[user.exercises.length-1].date,
-          _id: user._id.toString()
-        })
-      })
-      .catch( error => reject(error))
-    })
+    } catch (error) {
+      throw error;
+    }
   }
 
-  getUserLogs(id: string, from?: Date, to?: Date, limit?: number): Promise<ILogResponse> {
-    return new Promise((resolve, reject) => {
-      // If is invalid ID, abbort
-      if (!mongoose.isValidObjectId(id))
-      return reject(new Error("Invalid user ID."));
+  async getUserLogs(id: string, from?: Date, to?: Date, limit?: number): Promise<ILogResponse> {
+    let count: number = 0;
 
-      User.findById(id)
-      .then( user => {
-        if (!user)
-        return reject(new Error("User does not exist."));
+    // TODO: Put this validation on controllers
+    if (!mongoose.isValidObjectId(id)) 
+    throw new Error("Invalid user ID.");
+    
+    try {
+      const user = await User.findById(id)
+      let log: IExercise[] = user!.exercises;
 
-        let log: IExercise[];
-        let count: number = 0;
+      if (from && to && limit) {
+        log = log.filter(exercise => {
+          if (count >= limit) return false;
 
-        // Checks if exists a filter
-        if (from && to && limit) {
-          log = user.exercises.filter((exercise) => {
-            if (count >= limit) return false;
-              
-            if (Utils.dateIsBetween(new Date(exercise.date), from, to)) {
-              count++;
-              return true;
-            }
-          })
-        } else {
-          log = user.exercises
-        }
-
-        resolve({
-          username: user.username,
-          count: user.exercises.length,
-          _id: user._id.toString(),
-          log
+          if (Utils.dateIsBetween(new Date(exercise.date), from, to)) {
+            count++;
+            return true;
+          }
         })
-      })
-      .catch( error => reject(error))
-    })
+      }
+
+      return {
+        username: user!.username,
+        count: user!.exercises.length,
+        _id: user!._id.toString(),
+        log
+      }      
+
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
