edc4920c7660b71c05453b6fb38c225b9923e23b
unchainedDavid <afonso.script.sol@gmail.com>
2023-03-25 23:13:23 -0400

    refactor: removing promises from controllers

--- src/controller.ts
+++ src/controller.ts
@@ -1,66 +1,66 @@
 import { Request, Response } from "express";
-import databaseManager from "./services/DatabaseManager";
 
+import databaseManager from "./services/DatabaseManager";
 import Utils from "./utils";
 
 class Controller {
   databaseManager;
 
-  constructor() {
-    this.databaseManager = databaseManager
+  constructor(dataManager: typeof databaseManager) {
+    this.databaseManager = dataManager;
   }
 
-  createUser(request: Request, response: Response) {
+  async createUser(request: Request, response: Response) {
     const { username } = request.body;
     
     if (!username) 
     return response.status(400).send("No data provided.");
 
-    databaseManager.createUser(username)
-    .then( data => {
+    try {
+      const data = await databaseManager.createUser(username);
       return response.json(data);
-    })
-    .catch( error => {
-      console.log(error)
-      return response.status(500).send("Could not create the user.");
-    })
+    } catch (error) {
+      console.log(error);
+      return response.status(500).send("Could not create the user."); 
+    }
   }
 
-  getAllUsers(_: Request, response: Response) {
-    databaseManager.getAllUsers()
-    .then( data => {
-      return response.json(data)
-    })
-    .catch( error => {
-      return response.status(500).send("Could not retrieve users.");
-    })
+  async getAllUsers(_: Request, response: Response) {
+    try {
+      const data = await databaseManager.getAllUsers();
+      return response.json(data);
+    } catch (error) {
+      console.log(error);
+      return response.status(500).send("Could not retrieve users."); 
+    }
   }
 
-  saveExercise(request: Request, response: Response) {
+  async saveExercise(request: Request, response: Response) {
     const { description, duration, date } = request.body;
     const { _id } = request.params;
 
     let formatedDate: string | undefined;
 
-    if (!description && !duration && !_id)
+    if (!description || !duration || !_id)
     return response.status(400).send("Data missing.");
 
+    // TODO: Validate date and return formated in Utils
     if (date) {
       if (!Utils.isCorrectDate(new Date(date)))
       return response.status(400).send("Invalid date passed.");
       formatedDate = Utils.formatDateToString(new Date(date));
     }
     
-    databaseManager.saveUserExercise(_id, description, duration, date)
-    .then( data => {
+    try {
+      const data = await databaseManager.saveUserExercise(_id, description, duration, date);
       return response.json(data);
-    })
-    .catch( error => {
-      return response.status(500).send("Could not save user exercise.");
-    })
+    } catch (error) {
+      console.log(error);
+      return response.status(500).send("Could not save user exercise."); 
+    }
   }
 
-  getUserLogs(request: Request, response: Response) {
+  async getUserLogs(request: Request, response: Response) {
     const { _id } = request.params;
     const { from, to, limit } = request.query;
     
@@ -68,8 +68,7 @@ class Controller {
     let final: Date | undefined = undefined;
     let count: number | undefined = undefined;
 
-    if (!_id)
-    return response.status(400).send("Data missing.");
+    if (!_id) return response.status(400).send("Data missing.");
 
     // Check if entries are valid
     if (from && to && limit) {
@@ -89,14 +88,14 @@ class Controller {
       return response.status(400).send("Invalid limit passed.");
     }
     
-    databaseManager.getUserLogs(_id, initial, final, count)
-    .then( data => {
+    try {
+      const data = await databaseManager.getUserLogs(_id, initial, final, count);
       return response.json(data);
-    })
-    .catch( error => {
-      return response.status(400).send("Could not get user logs.");
-    })
+    } catch (error) {
+      console.log(error);
+      return response.status(400).send("Could not get user logs."); 
+    }
   }
 }
 
-export default new Controller();
+export default new Controller(databaseManager);
